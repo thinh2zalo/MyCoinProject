3f03ee91ee8e0b91f8a7d7ec977b125905e2d2ac
hoai thi <hoaithipm2@gmail.com>
2021-05-13 12:20:34 +0700

    add get to blockchain.js

--- Backend/blockchain.js
+++ Backend/blockchain.js
@@ -64,3 +64,75 @@ Blockchain.prototype.chainIsValid = function (blockchain) {
 
     return validChain;
 };
+
+Blockchain.prototype.getBlock = function (blockHash) {
+    let correctBlock = null;
+    this.chain.forEach(block => {
+        if (block.hash === blockHash)
+            correctBlock = block;
+    });
+    return correctBlock;
+};
+
+Blockchain.prototype.getTransaction = function (transactionId) {
+    let correctTransaction = null;
+    let correctBlock = null;
+
+    this.chain.forEach(block => {
+        block.transactions.forEach(transaction => {
+            if (transaction.transactionId === transactionId) {
+                correctTransaction = transaction;
+                correctBlock = block;
+            };
+        });
+    });
+
+    return {
+        transaction: correctTransaction,
+        block: correctBlock
+    };
+};
+
+
+Blockchain.prototype.getAddressInfor = function (address) {
+    const addressTransactions = [];
+    this.chain.forEach(block => {
+        block.transactions.forEach(transaction => {
+            if (transaction.sender === address || transaction.recipient === address) {
+                addressTransactions.push(transaction); 
+            };
+        });
+    },);
+
+    if (addressTransactions == null) {
+        return false;
+    }
+
+    var amountArr = [];
+    
+    let balance = 0;
+    addressTransactions.forEach(transaction => {
+        if (transaction.recipient === address) {
+            balance += transaction.amount;
+            amountArr.push(balance);
+        }
+        else if (transaction.sender === address) {
+            balance -= transaction.amount;
+            amountArr.push(balance);
+        }
+    
+    });
+
+    return {
+        addressTransactions: addressTransactions,
+        addressBalance: balance,
+        amountArr: amountArr
+    };
+};
+
+Blockchain.prototype.getTransactionsInQueue = function () {
+    return this.pendingTransactions;
+};
+
+
+module.exports = Blockchain;
